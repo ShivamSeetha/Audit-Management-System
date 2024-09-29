1d1186b4a361d3c52899fe80bb3331b6c723ec81
Shivam <shivamseetha0@gmail.com>
2024-09-29 18:24:53 +0530

    Uploading audit management project

---
+++ audit-authorization/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'Tanya garg', 'tg', 'tg000', 'Project-1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'sairam', 'ps', 'ss000', 'Project-2');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (3, 'shivam seetha', 'ss', 'ss000', 'Project-3');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (4, 'Girish goud', 'gg', 'gg000', 'Project-4');
+
